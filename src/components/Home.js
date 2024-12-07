c17bebb9dc5f0811f16c1e1250d3f96757e5b691
Morgan Larivain <morgan.larivain@yahoo.fr>
2024-12-07 12:19:57 -0500

    Amelioration animation debut chgt

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import '../styles/Home.css';
 import morganImage from '../assets/morgan.png';
 import { Link, Events } from 'react-scroll';
 import { useTranslation } from 'react-i18next';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   const { t } = useTranslation();
   const [activeLink, setActiveLink] = React.useState('home');
+  const [isLoading, setIsLoading] = useState(true);
 
   React.useEffect(() => {
     Events.scrollEvent.register('begin', function () { });
@@ -19,38 +21,125 @@ const Home = () => {
     };
   }, []);
 
+  // Simuler le temps de chargement
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 2000); // 2 secondes
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Animation de chargement
+  if (isLoading) {
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+        className="loading-screen"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          backgroundColor: 'var(--primary-bg)',
+        }}
+      >
+        <h1 style={{ color: 'var(--secondary-bg)', fontSize: '5rem', textAlign: 'center' }}>
+          ML
+        </h1>
+      </motion.div>
+    );
+  }
+
   return (
     <div id="home" className="section home-section">
       <div className="content-container">
         <div className="intro">
-          <h1>{t('hello')}</h1>
-          <p>{t('introduction')}</p>
+          {/* Animation sur le titre */}
+          <motion.h1
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.2 }} // Délais légers pour les éléments
+          >
+            {t('hello')}
+          </motion.h1>
+
+          {/* Animation sur le paragraphe */}
+          <motion.p
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.4 }}
+          >
+            {t('introduction')}
+          </motion.p>
+
           <div className="contact-buttons">
-            <a href="mailto:morgan.larivain@yahoo.fr" className="contact-icon" aria-label="Email">
+            <motion.a
+              href="mailto:morgan.larivain@yahoo.fr"
+              className="contact-icon"
+              aria-label="Email"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 0.6 }}
+            >
               <FaEnvelope />
-            </a>
-            <a href="https://www.linkedin.com/in/mlarivain/" className="contact-icon" aria-label="LinkedIn">
+            </motion.a>
+            <motion.a
+              href="https://www.linkedin.com/in/mlarivain/"
+              className="contact-icon"
+              aria-label="LinkedIn"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 0.8 }}
+            >
               <FaLinkedin />
-            </a>
-            <a href="https://github.com/morganlrvn" className="contact-icon" aria-label="GitHub">
+            </motion.a>
+            <motion.a
+              href="https://github.com/morganlrvn"
+              className="contact-icon"
+              aria-label="GitHub"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 1 }}
+            >
               <FaGithub />
-            </a>
+            </motion.a>
           </div>
 
-          <div className="about-more-button">
-            <Link to="projects" spy={true} hashSpy={true} smooth={true} duration={500} className={activeLink === 'projects' ? 'active-link' : ''} onSetActive={() => setActiveLink('projects')}>
+          {/* Animation sur le bouton "En savoir plus" */}
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}  // Départ en bas
+            animate={{ opacity: 1, y: 0 }}    // Devient visible et remonte
+            transition={{ duration: 1, delay: 1.2 }} // Délais un peu plus longs pour synchronisation
+            className="about-more-button"
+          >
+            <Link
+              to="projects"
+              spy={true}
+              hashSpy={true}
+              smooth={true}
+              duration={500}
+              className={activeLink === 'projects' ? 'active-link' : ''}
+              onSetActive={() => setActiveLink('projects')}
+            >
               {t('learn_more')}&emsp;&darr;
             </Link>
-          </div>
+          </motion.div>
         </div>
 
-        <div className="image-container">
+        {/* Animation sur l'image */}
+        <motion.div
+          initial={{ opacity: 0, scale: 0.8 }}  // Départ avec petite taille
+          animate={{ opacity: 1, scale: 1 }}    // Devient normal et visible
+          transition={{ duration: 1, delay: 0.5 }} // Délai léger avant de commencer l'animation
+          className="image-container"
+        >
           <img
             src={morganImage}
             alt="MorganL"
             className="home-image"
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
